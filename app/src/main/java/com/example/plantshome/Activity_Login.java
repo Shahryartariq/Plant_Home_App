7f631dbb00da6576a251288a672c82cef25f9347
Sherry <Shahryartariq57@gmail.com>
2021-05-02 22:10:23 +0500

    menu change and beauitfy

--- app/src/main/java/com/example/plantshome/Activity_Login.java
+++ app/src/main/java/com/example/plantshome/Activity_Login.java
@@ -1,14 +1,18 @@
 package com.example.plantshome;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Activity_Login extends AppCompatActivity {
     private Button btnLogin;
+    private ImageView btnfb;
+    private ImageView btngo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +30,27 @@ public class Activity_Login extends AppCompatActivity {
             }
 
         });
+
+        btnfb = findViewById(R.id.btnfb);
+        btnfb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url="https://www.facebook.com/";
+                Intent intentfb =new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intentfb);
+            }
+        });
+
+        btngo = findViewById(R.id.btngo);
+        btngo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url="https://accounts.google.com/servicelogin";
+                Intent intentgo =new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intentgo);
+            }
+        });
+
     }
     public void Navigate(){
         Intent intent = new Intent(this,Activity_Choices.class);
--- app/src/main/java/com/example/plantshome/Activity_Main.java
+++ app/src/main/java/com/example/plantshome/Activity_Main.java
@@ -1,7 +1,9 @@
 package com.example.plantshome;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 import android.widget.GridLayout;
@@ -11,6 +13,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.cardview.widget.CardView;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -34,13 +37,12 @@ public class Activity_Main extends AppCompatActivity {
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home,R.id.nav_report,R.id.nav_taskpanel,R.id.nav_badge,R.id.nav_feedback)
+                R.id.nav_home, R.id.nav_report, R.id.nav_taskpanel, R.id.nav_badge, R.id.nav_feedback)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
-
 
 
     }
@@ -60,14 +62,86 @@ public class Activity_Main extends AppCompatActivity {
                 || super.onSupportNavigateUp();
     }
 
-    public void sherry(View view){
-        Intent intent = new Intent(this,Activity_Detail.class);
+    public void sherry(View view) {
+        Intent intent = new Intent(this, Activity_Detail.class);
         startActivity(intent);
     }
 
     public void gotoThank(View view) {
-        Intent intentThank = new Intent(this,Activity_Thanks.class);
+        Intent intentThank = new Intent(this, Activity_Thanks.class);
         startActivity(intentThank);
 
     }
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId()) {
+            case R.id.search:
+                Toast
+                        .makeText(
+                                getApplicationContext(),
+                                "search",
+                                Toast.LENGTH_SHORT)
+                        .show();
+                return true;
+
+
+            case R.id.action_settings:
+                Toast
+                        .makeText(
+                                getApplicationContext(),
+                                "Settings",
+                                Toast.LENGTH_SHORT)
+                        .show();
+                return true;
+
+
+            case R.id.action_rate:
+                Toast
+                        .makeText(
+                                getApplicationContext(),
+                                "Rate Us",
+                                Toast.LENGTH_SHORT)
+                        .show();
+                return true;
+
+
+
+            case R.id.notification:
+                Toast
+                        .makeText(
+                                getApplicationContext(),
+                                "notification",
+                                Toast.LENGTH_SHORT)
+                        .show();
+                return (true);
+
+            case R.id.exit:
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+                alertDialogBuilder.setTitle("Exit Application?");
+                alertDialogBuilder
+                        .setMessage("Click yes to exit!")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        moveTaskToBack(true);
+                                        android.os.Process.killProcess(android.os.Process.myPid());
+                                        System.exit(1);
+                                    }
+                                })
+
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+
+                                dialog.cancel();
+                            }
+                        });
+
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
+                return (true);
+        }
+        return (super.onOptionsItemSelected(item));
+    }
+
 }
